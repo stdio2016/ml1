536ef1ebfa57c2533456c234fec9168e2d2354fa
stdio2016 <randomfrank8@gmail.com>
2017-10-31 15:56:06 +0800

    random forest runner

--- 1/validate.c
+++ 1/validate.c
@@ -6,6 +6,8 @@
 #include "id3.h"
 #include "validate.h"
 
+#define RANDOMFOREST_TREE 10
+
 struct id3_performance_t {
   float accuracy;
   float precision[CLASSCOUNT];
@@ -98,4 +100,96 @@ void kfold() {
   for (i = 0; i < CLASSCOUNT; i++) {
     printf("%.3f %.3f\n", sum.precision[i] / KFOLD_K, sum.recall[i] / KFOLD_K);
   }
+}
+
+int predictFromForest(int dataId, struct decision_tree **forest, int treeCount) {
+  int i;
+  int count[CLASSCOUNT] = {0};
+  for (i = 0; i < treeCount; i++) {
+    int ans = predictFromTree(dataId, forest[i]);
+    count[ans]++;
+  }
+  // find best result
+  int max = 0, maxtime = 1;
+  for (i = 1; i < CLASSCOUNT; i++) {
+    if (count[i] > count[max]) {
+      maxtime = 1;
+      max = i;
+    }
+    else if (count[i] == count[max]) {
+      maxtime++;
+    }
+  }
+  if (maxtime == 1) return max;
+  // there are more than one good results
+  // first tree with good result wins
+  for (i = 0; i < CLASSCOUNT; i++) {
+    int ans = predictFromTree(dataId, forest[i]);
+    if (count[ans] == count[max]) {
+      return ans;
+    }
+  }
+}
+
+void *randomForestRunner(void *param) {
+  int fold = *(int *)param;
+  int foldsize = DATASIZE / KFOLD_K;
+  // test dataset is [testStart, testEnd)
+  int testStart = fold * foldsize, testEnd = (fold + 1) * foldsize;
+  if (fold == KFOLD_K - 1) {
+    testEnd = DATASIZE;
+  }
+  // construct training dataset
+  int size = (DATASIZE - (testEnd - testStart));
+  int *training = malloc(sizeof(int) * size);
+  int i = 0;
+  for (i = 0; i < testStart; i++) {
+    training[i] = i;
+  }
+  for (i = testEnd; i < DATASIZE; i++) {
+    training[i - testEnd + testStart] = i;
+  }
+  // construct forest
+  struct decision_tree **forest= malloc(sizeof(struct decision_tree *) * RANDOMFOREST_TREE);
+  int *bag = malloc(sizeof(int) * size);
+  int allfeatures[FEATURECOUNT];
+  for (i = 0; i < FEATURECOUNT; i++) {
+    allfeatures[i] = i;
+  }
+  for (i = 0; i < RANDOMFOREST_TREE; i++) {
+    // pick training data, with replacement
+    int j;
+    for (j = 0; j < size; j++) {
+      int r = rand() % size;
+      bag[j] = training[r];
+    }
+    // build a tree
+    forest[i] = id3_from_data(bag, size, FEATURECOUNT,  allfeatures);
+  }
+  // validate
+  int correct[CLASSCOUNT] = {0,0,0};
+  int count[CLASSCOUNT] = {0,0,0};
+  int predcount[CLASSCOUNT] = {0,0,0};
+  for (i = testStart; i < testEnd; i++) {
+    int predict = predictFromForest(i, forest, RANDOMFOREST_TREE);
+    int actual = Target[i];
+    if (predict == actual) correct[actual]++;
+    count[actual]++;
+    predcount[predict]++;
+  }
+  int correctSum = 0;
+  for (i = 0; i < CLASSCOUNT; i++) {
+    correctSum += correct[i];
+    Perf[fold].precision[i] = correct[i] * 1.0 / predcount[i];
+    Perf[fold].recall[i] = correct[i] * 1.0 / count[i];
+  }
+  Perf[fold].accuracy = correctSum * 1.0 / foldsize;
+  // cleanup
+  for (i = 0; i < RANDOMFOREST_TREE; i++) {
+    freeTree(forest[i]);
+  }
+  free(forest);
+  free(bag);
+  free(training);
+  return NULL;
 }
