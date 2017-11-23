3e8ed730a37a2d917dc99e56229ac7c03b02f12c
stdio2016 <randomfrank8@gmail.com>
2017-11-24 01:33:29 +0800

    aaa

--- 2/main.py
+++ 2/main.py
@@ -1,5 +1,17 @@
 import csv
 import sys
+import collections
+
+KdtreeNode = collections.namedtuple('KdtreeNode', ['center', 'left', 'right', 'size'])
+
+# only some of the columns in csv are used
+# and I need to convert string to number
+def selectAttributes(row):
+    selected = []
+    for i in range(2, 11):
+        selected.append(float(row[i])) # attributes
+    selected.append(row[11]) # class
+    return selected
 
 def main():
     if (len(sys.argv) < 3):
@@ -10,31 +22,37 @@ def main():
     with open(traincsv, 'r') as csvfile:
         reader = csv.reader(csvfile)
         next(reader) # skip first line
-        dat = [row for row in reader]
-        Kdtree(dat, 9, 0)
+        dat = [selectAttributes(row) for row in reader]
+        showKdtree(Kdtree(dat, 9, 0))
 
-class Kdtree:
-    def __init__(self, points, k, depth):
-        n = len(points)
-        self.size = n
-        if n == 1:
-            self.center = points[0]
-            self.left = None
-            self.right = None
-            return
+def Kdtree(points, k, depth):
+    n = len(points)
+    if n == 0:
+        return None
 
-        # select axis by depth
-        axis = depth % k
-        # sort by axis
-        A = sorted(points, key=lambda x: x[axis])
+    # select axis by depth
+    axis = depth % k
+    # sort by axis
+    A = sorted(points, key=lambda x: x[axis])
 
-        middle = n//2
-        self.center = A[middle]
-        self.left = Kdtree(A[0:middle], k, depth+1)
-        # boundary case
-        if n <= 2:
-            self.right = None
-        else:
-            self.right = Kdtree(A[middle+1:], k, depth+1)
+    middle = n//2
+    return KdtreeNode(
+        size = n,
+        center = A[middle],
+        left = Kdtree(A[:middle], k, depth+1),
+        right = Kdtree(A[middle+1:], k, depth+1)
+    )
+
+def showKdtree(tree, lv=0):
+    if tree == None:
+        print ("    " * lv + "None")
+    else:
+        print ("    " * lv + str(tree.center))
+        showKdtree(tree.left, lv+1)
+        showKdtree(tree.right, lv+1)
+
+# find k nearest neighbors of the query point
+def knn(k, query, dimension):
+    return 
 
 main()
